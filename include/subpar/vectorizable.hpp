3d72b5e54e65f3838fae1f896d6462521d01d4b8
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-10-29 23:25:50 -0700

    Mention restrictions on using the VECTORIZABLE macro.

--- include/subpar/vectorizable.hpp
+++ include/subpar/vectorizable.hpp
@@ -15,10 +15,15 @@
 #define SUBPAR_VECTORIZABLE _Pragma("loop(ivdep)")
 #else
 /**
- * Indicate a loop can be safely vectorized by promising that there are no dependencies between loop iterations.
+ * Indicate a `for` loop can be safely vectorized by promising that there are no dependencies between loop iterations.
  * This should be added above the loop and is replaced by a compiler-specific pragma.
  * Use of this macro does not force vectorization of the loop;
  * the decision to vectorize or not is left to the compiler's cost model.
+ *
+ * Needless to say, this macro should only be used when the loop iterations are independent.
+ * The loop body should not perform any reductions or modify any shared values.
+ * (Assignments to different values of the same array are obviously allowed.)
+ * To be safe, the loop statements should only modify a single variable, namely the counter or iterator used to define the loop.
  */
 #define SUBPAR_VECTORIZABLE
 #endif
